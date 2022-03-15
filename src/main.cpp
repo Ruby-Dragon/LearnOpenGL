f44e586acff52ddae605d2a1670f077431fce557
Ruby-Dragon <91497228+Ruby-Dragon@users.noreply.github.com>
2022-03-15 11:00:50 -0700

    rainbow triangle

--- src/main.cpp
+++ src/main.cpp
@@ -69,43 +69,36 @@ int main()
 	//END SHADERS
 
 	//triangle
-	float vertices[] = {
-			0.5f,  0.5f, 0.0f,  // top right
-			0.5f, -0.5f, 0.0f,  // bottom right
-			-0.5f, -0.5f, 0.0f,  // bottom left
-			-0.5f,  0.5f, 0.0f   // top left
-	};
-
-	unsigned int indices[] = {  // note that we start from 0!
-			0, 1, 3,   // first triangle
-			1, 2, 3    // second triangle
-	};
+    float vertices[] = {
+            // positions                // colors
+            0.5f, -0.5f,      0.0f,  1.0f, 0.0f, 0.0f,   // bottom right
+            -0.5f, -0.5f,     0.0f,  0.0f, 1.0f, 0.0f,   // bottom left
+            0.0f,  0.5f,    0.0f,  0.0f, 0.0f, 1.0f    // top
+    };
 
 	//make VBO
 	unsigned int VBO;
+    //VAO
+    unsigned int VAO;
+    glGenVertexArrays(1, &VAO);
+    //VBO
 	glGenBuffers(1, &VBO);
-
-	//make VAO
-	unsigned int VAO;
-	glGenVertexArrays(1, &VAO);
-
-	//make EBO
-	unsigned int EBO;
-	glGenBuffers(1, &EBO);
-
+    //Bind VAO
 	glBindVertexArray(VAO);
-
-	//bind buffers to EBO
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
 	//Bind buffers to VBO
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
 	//set vertex attributes pointer
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-	glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+    // color attribute
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3* sizeof(float)));
+    glEnableVertexAttribArray(1);
+
+    glUseProgram(shaderProgram);
+
 
 	//render loop
 	while(!glfwWindowShouldClose(window))
@@ -113,24 +106,17 @@ int main()
 		//process window input
 		processInput(window);
 
-        float timeValue = glfwGetTime();
-        float greenValue = (sin(timeValue) / 2.0f) + 0.5f;
-        int vertexColorLocation = glGetUniformLocation(shaderProgram, "ourColor");
-        glUseProgram(shaderProgram);
-        glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
+        //clear screen
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
 
-		glUseProgram(shaderProgram);
+        //shaders run
 		glBindVertexArray(VAO);
-		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-		glBindVertexArray(0);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
 		//render to screen
 		glfwSwapBuffers(window);
 		glfwPollEvents();
-
-		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT);
 	}
 
 	glfwTerminate();
