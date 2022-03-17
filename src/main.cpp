f345d90dbfec08cd797dd843250c3f3eb7034c87
Ruby-Dragon <91497228+Ruby-Dragon@users.noreply.github.com>
2022-03-17 10:54:55 -0700

    textured rectangle

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include "glad/glad.h"
 #include "GLFW/glfw3.h"
 #include "shader.hpp"
+#include "image/stb_image.h"
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 
@@ -44,23 +45,29 @@ int main()
 
 	//triangle
     float vertices[] = {
-            // positions                // colors
-            0.5f, -0.5f,      0.0f,  1.0f, 0.0f, 0.0f,   // bottom right
-            -0.5f, -0.5f,     0.0f,  0.0f, 1.0f, 0.0f,   // bottom left
-            0.0f,  0.5f,    0.0f,  0.0f, 0.0f, 1.0f    // top
+            // positions          // colors                                  // texture coords
+            0.5f,  0.5f,    0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f,   // top right
+            0.5f, -0.5f,    0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f,   // bottom right
+            -0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f,   // bottom left
+            -0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f    // top left
     };
 
-    float texCoords[] = {
-            0.0f, 0.0f,  // lower-left corner
-            1.0f, 0.0f,  // lower-right corner
-            0.5f, 1.0f   // top-center corner
+    unsigned int indices[] = {  // note that we start from 0!
+            0, 1, 3,   // first triangle
+            1, 2, 3    // second triangle
     };
 
-	//make VBO
+    //make VBO
 	unsigned int VBO;
+
     //VAO
     unsigned int VAO;
     glGenVertexArrays(1, &VAO);
+
+    //EBO
+    unsigned int EBO;
+    glGenBuffers(1, &EBO);
+
     //VBO
 	glGenBuffers(1, &VBO);
     //Bind VAO
@@ -70,17 +77,47 @@ int main()
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
+    //EBO
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
 	//set vertex attributes pointer
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
     // color attribute
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3* sizeof(float)));
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3* sizeof(float)));
     glEnableVertexAttribArray(1);
+    //texcoords
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+    glEnableVertexAttribArray(2);
 
     glUseProgram(ourShader.ID);
 
+    //TEXTURES
+    unsigned int texture;
+    glGenTextures(1, &texture);
+    glBindTexture(GL_TEXTURE_2D, texture);
+// set the texture wrapping/filtering options (on the currently bound texture object)
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+// load and generate the texture
+    int width, height, nrChannels;
+    unsigned char *data = stbi_load("textures/container.jpg", &width, &height, &nrChannels, 0);
+    if (data)
+    {
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    }
+    else
+    {
+        std::cout << "Failed to load texture" << std::endl;
+    }
+    stbi_image_free(data);
+    //END TEXTURES
 
-	//render loop
+    //render loop
 	while(!glfwWindowShouldClose(window))
 	{
 		//process window input
@@ -91,8 +128,10 @@ int main()
         glClear(GL_COLOR_BUFFER_BIT);
 
         //shaders run
-		glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glBindTexture(GL_TEXTURE_2D, texture);
+        glBindVertexArray(VAO);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
+        glBindVertexArray(0);
 
 		//render to screen
 		glfwSwapBuffers(window);
